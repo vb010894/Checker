5a1883c20c7e2b1e2940dd8de12de6e5cbcd8db7
vd.zinovev <SamFisher010894@pc>
2022-04-26 10:31:41 +0300

    SSM 02 test

--- checker-tests/checker-tests-ssm/src/main/java/ru/checker/tests/ssm/SAPSSM.java
+++ checker-tests/checker-tests-ssm/src/main/java/ru/checker/tests/ssm/SAPSSM.java
@@ -1,16 +1,12 @@
 package ru.checker.tests.ssm;
 
 import lombok.extern.log4j.Log4j2;
-import mmarquee.automation.controls.Panel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 import org.springframework.stereotype.Component;
 import ru.checker.tests.ssm.base.SSMTestCase;
-import ru.checker.tests.ssm.controls.grid.SSMGrid;
-import ru.checker.tests.ssm.controls.grid.SSMGridData;
-import ru.checker.tests.ssm.widgets.controllers.SSMToolsController;
-
-import static org.junit.jupiter.api.Assertions.*;
+import ru.checker.tests.ssm.tests.SSMSapTests;
 
 /**
  * SSM 'SAP Orders' form testing.
@@ -33,51 +29,22 @@ public class SAPSSM extends SSMTestCase {
     @DisplayName("ТС.SSM.01.Заказы SAP. Работа с фильтрами")
     @Test
     void ssm01() {
-
-        Panel sap_order_grid = this.getForm().panel("ssm_01_01", -1);
-        SSMGrid grid = new SSMGrid(sap_order_grid);
-        SSMToolsController menu = this.getRootWindow().widget("ssm_menu", SSMToolsController.class);
-        menu.toggle("ssm_01", false);
-        SSMGridData data = grid.getAllData();
-        assertEquals(data.getRowSize(), 0L, "Таблица 'Заказы SAP' не пуста");
-        menu.toggle("ssm_01", true);
-        data = grid.getAllData();
-        assertNotEquals(data.getRowSize(), 0L, "Таблица 'Заказы SAP' пуста");
-        assertTrue(
-                grid.columnExistValue("С", "Открыт"),
-                "В таблице 'Заказы SAP' найдено значение в колонке 'C' отличное от 'Открыт'. ID - 'ssm_01_01'");
-        menu.toggle("ssm_01", false);
-        menu.toggle("ssm_02", true);
-        data = grid.getFirstPageData();
-        assertNotEquals(data.getRowSize(), 0L, "Таблица 'Заказы SAP' пуста");
-        assertTrue(
-                grid.columnExistValue("С", "Закрыт"),
-                "В таблице 'Заказы SAP' найдено значение в колонке 'C' отличное от 'Закрыт'. ID - 'ssm_01_01'");
-
-        menu.toggle("ssm_02", false);
-        menu.toggle("ssm_01", true);
-
-        menu.selectCombobox("ssm_03", "РМЦ-1");
-        grid.getAllData();
-        assertTrue(
-                grid.columnExistValue("Цех", "РМЦ-1"),
-                "В таблице 'Заказы SAP' найдено значение в колонке 'Цех' отличное от 'РМЦ-1'. ID - 'ssm_01_01'");
-        menu.selectCombobox("ssm_03", "КПЦ");
-        grid.getAllData();
-        assertTrue(
-                grid.columnExistValue("Цех", "КПЦ"),
-                "В таблице 'Заказы SAP' найдено значение в колонке 'Цех' отличное от 'КПЦ'. ID - 'ssm_01_01'");
-        menu.selectCombobox("ssm_03", "ЦРМО-1");
-        grid.getAllData();
-        assertTrue(
-                grid.columnExistValue("Цех", "ЦРМО-1"),
-                "В таблице 'Заказы SAP' найдено значение в колонке 'Цех' отличное от 'ЦРМО-1'. ID - 'ssm_01_01'");
+        SSMSapTests.SSM01(this.getRootWindow(), this.getForm());
       /*  SSMToolsMenu menu = new SSMToolsMenu(filterPZ);
         menu.clickOnField("Закрытые");*/
        /* CheckerDesktopWindow prb = getSApplication().window("sap_oreder_prb_form");
         Button b = prb.getControl().getButton("Отмена");
         b.click();*/
      //   prb.getWindow().close();
+    }
+
+    /**
+     * ТС.SSM.02 test
+     */
+    @DisplayName("ТС.SSM.01.Заказы SAP. Работа с фильтрами")
+    @Test
+    void ssm02() {
+        SSMSapTests.SSM02(this.getRootWindow(), this.getForm());
     }
 
 
