e0abacce2086371ac01c3aa30191b2f14aaa4f43
vd.zinovev <SamFisher010894@pc>
2022-07-27 15:56:15 +0300

    Изменено 1) Размножен тест #113144

--- checker-tests/checker-tests-ssm/src/main/java/ru/checker/tests/ssm/tests/product/SSMProductReleaseTest.java
+++ checker-tests/checker-tests-ssm/src/main/java/ru/checker/tests/ssm/tests/product/SSMProductReleaseTest.java
@@ -15,4 +15,18 @@ public class SSMProductReleaseTest extends SSMTest {
        new SSMG01P01(getRootWindow(), "product_release").run();
     }
 
+    @Test(
+            testName = "ТС.SSM.G.01.01.P.01",
+            description = "Выпуск продукции SAP. Работа с фильтрами 2")
+    public void SSMG0101P012() {
+        new SSMG01P01(getRootWindow(), "product_release").run();
+    }
+
+    @Test(
+            testName = "ТС.SSM.G.01.01.P.01",
+            description = "Выпуск продукции SAP. Работа с фильтрами 3")
+    public void SSMG0101P013() {
+        new SSMG01P01(getRootWindow(), "product_release").run();
+    }
+
 }
